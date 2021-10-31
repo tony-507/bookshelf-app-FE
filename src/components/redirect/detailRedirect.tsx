d3793990126a21f23862029a47e16ca1ad462fd7
tony-507 <tony57507@gmail.com>
2021-10-31 20:51:02 +0800

    Remove unnecessary files

--- src/components/redirect/detailRedirect.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import { Redirect, useParams } from "react-router-dom"
-
-export const BookDetailRedirect = () => {
-	let id = useParams()
-
-	return <Redirect to={`/books/${id}`} />
-}
