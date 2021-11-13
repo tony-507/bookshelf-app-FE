97b30fc264b9fa35face39c4ff3f4dfa1af8a5a0
tony-507 <tony57507@gmail.com>
2021-11-13 22:53:59 +0800

    Webpack setup

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,7 +1,7 @@
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
 
-module.exports = {
+module.exports = merge(common, {
     mode: "development",
 
     devtool: "inline-source-map", // Source map allows one to debug output: Refer error to original script not bundle.js
@@ -11,4 +11,4 @@ module.exports = {
         hot: true, // Colored output in console
         historyApiFallback: true
     },
-};
+});
